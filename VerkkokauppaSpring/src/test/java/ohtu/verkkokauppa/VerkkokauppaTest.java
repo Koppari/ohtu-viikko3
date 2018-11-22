4ab1e9e7cb742ee5d86035f33b0da0ae0e96ed94
Koppari <robin.stachon@helsinki.fi>
2018-11-22 05:02:49 +0200

    t5

--- VerkkokauppaSpring/src/test/java/ohtu/verkkokauppa/VerkkokauppaTest.java
+++ VerkkokauppaSpring/src/test/java/ohtu/verkkokauppa/VerkkokauppaTest.java
@@ -103,7 +103,7 @@ public class VerkkokauppaTest {
         // määritellään että viitegeneraattori palauttaa viitten 42
         when(viite.uusi()).thenReturn(42);
 
-        // määritellään että tuote numero 1 on maito jonka hinta on 7 ja saldo 10, 
+        // määritellään että tuote numero 1 on maito jonka hinta on 7 ja saldo 10,
         // tuote numero 2 on puuro jonka hinta on 5 saldo on 0
         when(varasto.saldo(1)).thenReturn(10);
         when(varasto.saldo(2)).thenReturn(0);
@@ -144,7 +144,7 @@ public class VerkkokauppaTest {
 
     @Test
     public void kauppaPyytaaUudenViitenumeron() {
-        // määritellään että viitegeneraattori palauttaa viitteen 42 sekä 43
+        // määritellään että viitegeneraattori palauttaa viitteen 42 ja 43
         when(viite.uusi()).thenReturn(42).thenReturn(43);
 
         // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
