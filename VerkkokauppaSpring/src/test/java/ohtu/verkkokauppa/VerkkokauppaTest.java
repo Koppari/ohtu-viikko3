d4f0bf16f8daff538d0bb1d1fefb38078b2e4731
Koppari <robin.stachon@helsinki.fi>
2018-11-22 04:56:13 +0200

    t5

--- VerkkokauppaSpring/src/test/java/ohtu/verkkokauppa/VerkkokauppaTest.java
+++ VerkkokauppaSpring/src/test/java/ohtu/verkkokauppa/VerkkokauppaTest.java
@@ -54,7 +54,7 @@ public class VerkkokauppaTest {
         k.tilimaksu("pekka", "12345");
 
         // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
-        verify(pankki).tilisiirto("pekka", 42, "12345", "33333-44455", 5);
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), eq("33333-44455"), eq(5));
     }
 
     @Test
@@ -63,6 +63,7 @@ public class VerkkokauppaTest {
         when(viite.uusi()).thenReturn(42);
 
         // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
+        // tuote numero 2 on puuro jonka hinta on 4 ja saldo 10
         when(varasto.saldo(1)).thenReturn(10);
         when(varasto.saldo(2)).thenReturn(10);
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
@@ -75,7 +76,7 @@ public class VerkkokauppaTest {
         k.tilimaksu("pekka", "12345");
 
         // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
-        verify(pankki).tilisiirto("pekka", 42, "12345", "33333-44455", 9);
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), eq("33333-44455"), eq(9));
     }
 
     @Test
@@ -94,7 +95,7 @@ public class VerkkokauppaTest {
         k.tilimaksu("pekka", "12345");
 
         // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
-        verify(pankki).tilisiirto("pekka", 42, "12345", "33333-44455", 10);
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), eq("33333-44455"), eq(10));
     }
 
     @Test
@@ -102,7 +103,8 @@ public class VerkkokauppaTest {
         // määritellään että viitegeneraattori palauttaa viitten 42
         when(viite.uusi()).thenReturn(42);
 
-        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
+        // määritellään että tuote numero 1 on maito jonka hinta on 7 ja saldo 10, 
+        // tuote numero 2 on puuro jonka hinta on 5 saldo on 0
         when(varasto.saldo(1)).thenReturn(10);
         when(varasto.saldo(2)).thenReturn(0);
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 7));
@@ -115,7 +117,73 @@ public class VerkkokauppaTest {
         k.tilimaksu("pekka", "12345");
 
         // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
-        verify(pankki).tilisiirto("pekka", 42, "12345", "33333-44455", 7);
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), eq("33333-44455"), eq(7));
+    }
+
+    @Test
+    public void aloitaAsiointiNollaaEdellisenOstoksenTiedot() {
+        // määritellään että viitegeneraattori palauttaa viitteen 42
+        when(viite.uusi()).thenReturn(42);
+
+        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1); // ostetaan tuotetta numero 1 eli maitoa
+
+        // tehdään ostokset uudelleen
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1); // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("pekka", "12345");
+
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), eq("33333-44455"), eq(5));
+    }
+
+    @Test
+    public void kauppaPyytaaUudenViitenumeron() {
+        // määritellään että viitegeneraattori palauttaa viitteen 42 ja 43
+        when(viite.uusi()).thenReturn(42).thenReturn(43);
+
+        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1); // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("pekka", "12345");
+
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), eq("33333-44455"), eq(5));
+
+        // tehdään ostokset uudelleen, toivottavasti uudella viitenumerolla
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1); // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("pekka", "12345");
+
+        // uusi tilisiirtovarmistus, varmistetaan viitenumero 43
+        verify(pankki).tilisiirto(eq("pekka"), eq(43), eq("12345"), eq("33333-44455"), eq(5));
+    }
+
+    @Test
+    public void koristaPoistoKutsuuMetodejaHaeTuoteJaPalauta() {
+        Tuote maito = new Tuote(1, "maito", 5);
+        // määritellään että viitegeneraattori palauttaa viitteen 42
+        when(viite.uusi()).thenReturn(42);
+
+        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(maito);
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1); // ostetaan tuotetta numero 1 eli maitoa
+        k.poistaKorista(1);
+
+        verify(varasto, times(1)).palautaVarastoon(maito);
     }
 
 }
