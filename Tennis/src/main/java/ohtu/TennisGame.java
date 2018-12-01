9039cd7b22f8018c4fdf3d4725214f68cf945f90
Koppari <robin.stachon@helsinki.fi>
2018-12-01 07:23:44 +0200

    tehtava 2 done

--- Tennis/src/main/java/ohtu/TennisGame.java
+++ Tennis/src/main/java/ohtu/TennisGame.java
@@ -1,80 +1,69 @@
 package ohtu;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TennisGame {
-    
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
 
-    public TennisGame(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
-    }
+    private String score;
+    private String playerOne;
+    private String playerTwo;
+    private int playerOneScore = 0;
+    private int playerTwoScore = 0;
 
-    public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+    private final Map<Integer, String> SCORES = new HashMap<Integer, String>() {
+        {
+            put(0, "Love");
+            put(1, "Fifteen");
+            put(2, "Thirty");
+            put(3, "Forty");
+        }
+    };
+
+    public TennisGame(String playerOne, String playerTwo) {
+        this.score = "";
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
+        if (playerOneScore == playerTwoScore)
+            tie();
+        else if (playerOneScore >= 4 || playerTwoScore >= 4)
+            gamePoint();
         else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-        }
+            playerScores();
         return score;
     }
+
+    public void wonPoint(String playerWhoScored) {
+        if (playerWhoScored == playerOne)
+            playerOneScore++;
+        else
+            playerTwoScore++;
+    }
+
+    public void tie() {
+        if (playerOneScore < 4)
+            score = SCORES.get(playerOneScore) + "-All";
+        else
+            score = "Deuce";
+    }
+
+    public void playerScores() {
+        score += SCORES.get(playerOneScore) + "-" + SCORES.get(playerTwoScore);
+    }
+
+    public void gamePoint() {
+        int scoreDifference = playerOneScore - playerTwoScore;
+        if (scoreDifference == 1)
+            score = "Advantage " + playerOne;
+        else if (scoreDifference == -1)
+            score = "Advantage " + playerTwo;
+        else if (scoreDifference >= 2)
+            score = "Win for " + playerOne;
+        else
+            score = "Win for " + playerTwo;
+    }
+
 }
